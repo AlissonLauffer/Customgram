c2b322a0d9ceba216e7cedbc4245fd821002bd0b
Alisson Lauffer <alissonvitortc@gmail.com>
2019-08-24 01:11:44 -0300

    Fix an issue

--- TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/LocaleController.java
@@ -1570,35 +1570,39 @@ public class LocaleController {
     }
 
     public static String formatShortNumber(int number, int[] rounded) {
-        String number_count = number.toString();
-        return number_count;
-        StringBuilder K = new StringBuilder();
-        int lastDec = 0;
-        int KCount = 0;
-        while (number / 1000 > 0) {
-            K.append("K");
-            lastDec = (number % 1000) / 100;
-            number /= 1000;
-        }
-        if (rounded != null) {
-            double value = number + lastDec / 10.0;
-            for (int a = 0; a < K.length(); a++) {
-                value *= 1000;
-            }
-            rounded[0] = (int) value;
-        }
-        if (lastDec != 0 && K.length() > 0) {
-            if (K.length() == 2) {
-                return String.format(Locale.US, "%d.%dM", number, lastDec);
-            } else {
-                return String.format(Locale.US, "%d.%d%s", number, lastDec, K.toString());
-            }
-        }
-        if (K.length() == 2) {
-            return String.format(Locale.US, "%dM", number);
-        } else {
-            return String.format(Locale.US, "%d%s", number, K.toString());
-        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("");
+        sb.append(number);
+        String number_str = sb.toString();
+        return number_str;
+
+//        StringBuilder K = new StringBuilder();
+//        int lastDec = 0;
+//        int KCount = 0;
+//        while (number / 1000 > 0) {
+//            K.append("K");
+//            lastDec = (number % 1000) / 100;
+//            number /= 1000;
+//        }
+//        if (rounded != null) {
+//            double value = number + lastDec / 10.0;
+//            for (int a = 0; a < K.length(); a++) {
+//                value *= 1000;
+//            }
+//            rounded[0] = (int) value;
+//        }
+//        if (lastDec != 0 && K.length() > 0) {
+//            if (K.length() == 2) {
+//                return String.format(Locale.US, "%d.%dM", number, lastDec);
+//            } else {
+//                return String.format(Locale.US, "%d.%d%s", number, lastDec, K.toString());
+//            }
+//        }
+//        if (K.length() == 2) {
+//            return String.format(Locale.US, "%dM", number);
+//        } else {
+//            return String.format(Locale.US, "%d%s", number, K.toString());
+//        }
     }
 
     public static String formatUserStatus(int currentAccount, TLRPC.User user) {
